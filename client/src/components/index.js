0db0f468e67e901b6603b367cc9a27b5f834a0d9
Priyanshu Dharmshaktu <dharmshaktu23@gmail.com>
2024-07-27 16:39:52 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+import Aipickker from "./Aipickker";
+
+export { CustomButton, ColorPicker, Aipickker, FilePicker, Tab };
